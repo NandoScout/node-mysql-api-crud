05b19e2216eabb9ce0a0f8d45d8273e691e4bd51
Fernando Broqua <ferbroqua@gmail.com>
2020-12-05 07:39:04 -0300

    url entry comments

--- contactosRoutes.js
+++ contactosRoutes.js
@@ -4,15 +4,15 @@ const router = express.Router();
 const contactosController = require('./contactosController');
 
 // app.use("/contactoss",router);
-router.get("/", contactosController.getAllContacts );
-router.get("/:id", contactosController.getOneContact );
-router.post("/", contactosController.addContact );
-router.put("/:id", contactosController.modifyContact );
-router.patch("/:id", contactosController.modifyContact );
-router.delete("/:id", contactosController.deleteContact );
+router.get("/", contactosController.getAllContacts );   // GET http://fbroqua.sytes.net/contacts
+router.get("/:id", contactosController.getOneContact ); // GET http://fbroqua.sytes.net/contacts/11
+router.post("/", contactosController.addContact );      // POST http://fbroqua.sytes.net/contacts
+router.put("/:id", contactosController.modifyContact ); // PUT http://fbroqua.sytes.net/contacts/11
+router.patch("/:id", contactosController.modifyContact ); // PATCH http://fbroqua.sytes.net/contacts/11
+router.delete("/:id", contactosController.deleteContact ); // DELETE http://fbroqua.sytes.net/contacts/11
 
-router.post("/agregar", contactosController.addContact );
-router.put("/modificar/:id", contactosController.modifyContact );
-router.delete("/eliminar/:id", contactosController.deleteContact );
+router.post("/agregar", contactosController.addContact ); // POST http://fbroqua.sytes.net/contacts/agregar
+router.put("/modificar/:id", contactosController.modifyContact );  // PUT http://fbroqua.sytes.net/contacts/modificar/11
+router.delete("/eliminar/:id", contactosController.deleteContact ); // DELETE http://fbroqua.sytes.net/contacts/eliminar/11
  
 module.exports = router;
